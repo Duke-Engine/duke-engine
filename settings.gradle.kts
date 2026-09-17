eebb51f503b0974fae2540da902bf4289b8e582c
Abdurasul Abduraimov <abdurasulabduraimov22@gmail.com>
2026-09-17 11:21:50 +0500

    removed redundant modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,3 @@
 rootProject.name = "duke-engine"
 
-include("core", "rts", "generals", "game", "client3d", "sandbox", "sandbox3d", "studio", "dungeon", "worldbuilder")
+include("core", "rts", "game", "client3d",  "studio", "dungeon", "worldbuilder")
